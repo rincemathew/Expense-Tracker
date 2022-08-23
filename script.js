bcb0f336eb506ade3370abdb1417e3b703a95ebb
Rince Mathew <rincemathewmanattu@gmail.com>
2022-08-23 13:28:45 +0530

    display balance,income,expense

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const money_plus = document.getElementById('money-plus');
 const money_minus = document.getElementById('money-minus');
 const list = document.getElementById('list');
 const form = document.getElementById('form');
-const text = document.getElementById('text');
+const form_expense = document.getElementById('form-expense');
 const amount = document.getElementById('amount');
 
 const dummyTransactions = [
@@ -15,34 +15,43 @@ const dummyTransactions = [
 
 let transactions = dummyTransactions;
 
-// Add transactions to DOM list
 function addTransactionDOM(transaction) {
-    // Get sign
     const sign = transaction.amount < 0 ? '-' : '+';
   
     const item = document.createElement('li');
   
-    // Add class based on value
     item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
   
     item.innerHTML = `
-      ${transaction.text} <span>${sign}${Math.abs(
-      transaction.amount
-    )}</span> <button class="delete-btn" onclick="removeTransaction(${
-      transaction.id
-    })">x</button>
+      ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}
+      </span> <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
     `;
   
     list.appendChild(item);
   }
 
-  // Init app
+function updateValues() {
+  const amounts = transactions.map(transaction => transaction.amount);
+  console.log(amounts)
+  const total = amounts.reduce((acc, item)=> (acc += item), 0).toFixed(2);
+  console.log(total)
+  const income = amounts.filter(item => item > 0).reduce((acc, item) => (acc += item), 0).toFixed(2);
+  console.log(income)
+  const expense = (amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1).toFixed(2);
+  console.log(expense)
+
+  balance.innerHTML =  `₹${total}`;
+  money_plus.innerHTML =  `₹${income}`;
+  money_minus.innerHTML =  `₹${expense}`;
+}
+
+
 function init() {
     list.innerHTML = '';
   
     transactions.forEach(addTransactionDOM);
-    
+    updateValues();
   }
   
-  init();
+init();
 
